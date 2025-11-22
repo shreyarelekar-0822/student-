c555c8fcf80ed9e6fd8d1aed52554a2b235a0883
shreyarelekar-0822 <shreyarelekar0822@gmail.com>
2025-11-22 11:38:40 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,13 +1,13 @@
-impoet sys
+import sys
 if len(sys.avgv)!=3:
   print("usage:python student.py<name><rollno>")
   sys.exit(1)
   #sys.argv[0]is always the program name
 script_name = sys.argv[0]
-name=sys.avgv[2]
+name=sys.avgv[1]
 
 print("script Name:",script_name)
 print("student Name:",name)
-print("roll Name:",rollno)
+
 
   
