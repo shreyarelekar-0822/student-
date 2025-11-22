22301e0a4b025f194187a87d19d304cc5aface21
shreyarelekar-0822 <shreyarelekar0822@gmail.com>
2025-11-22 09:47:45 +0530

    Implement student information script
    Fixes import statement and argument handling.

---
+++ student.py
@@ -0,0 +1,13 @@
+impoet sys
+if len(sys.avgv)!=3:
+  print("usage:python student.py<name><rollno>")
+  sys.exit(1)
+  #sys.argv[0]is always the program name
+script_name = sys.argv[0]
+name=sys.avgv[2]
+
+print("script Name:",script_name)
+print("student Name:",name)
+print("roll Name:",rollno)
+
+  
